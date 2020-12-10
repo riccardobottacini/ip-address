bcd05b66aade4d454558bc41384f94e498ddd06e
lorenzocardone <lorenzo.cardone@student.h-farm.com>
2020-12-10 15:42:41 +0100

    added first implementatio of argparse

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import ip_address
 import csv_manager
+
 IP_address = "1.2.3.4"
 csv_path = "data/ip_data.csv"
+
+def parse_arguments():
+    parser=argparse.ArgumentParser(
+            description="IP ADDRESS prject")
+    parser.add_argument ("ip_address", type=str,
+                         help= "Insert an IP Address",
+                         default=None)
 
 if __name__ == "__main__":
     city, country = ip_address.get_location(IP_address)
